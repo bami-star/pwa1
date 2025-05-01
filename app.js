416bf36f11274d4d08a2aa0811492031598c4133
bami-star <beamlak561@gmail.com>
2025-05-01 14:45:20 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js')
+    .then(registration => {
+      console.log('Service Worker registered with scope:', registration.scope);
+    })
+    .catch(error => {
+      console.error('Service Worker registration failed:', error);
+    });
+}
 // JS file for cycleTracker app
 
 // -------------
